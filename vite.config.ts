45e0870521e0c3ac21c0e5d0643853fe2a9ec02c
ayelencorrotea <101226466+ayelencorrotea@users.noreply.github.com>
2023-08-25 17:12:16 -0400

    cambio

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  vuetify({ autoImport: true }), // Enabled by default
+  plugins: [
+    vue(),
+    vuetify({ autoImport: true })
+  ],
 })
